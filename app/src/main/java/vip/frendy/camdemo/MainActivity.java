d5af8b51820e6f576976830a8cffe6fc6dc4d3c2
frendy.xu <frendy.xu@cootek.cn>
2018-04-08 10:06:42 +0800

    first commit

---
+++ app/src/main/java/vip/frendy/camdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package vip.frendy.camdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by frendy on 2018/4/8.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
